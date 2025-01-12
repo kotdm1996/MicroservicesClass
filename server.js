d602b5a2f9b60ccc8a3882a154c79166e678f1a9
kotdm1996 <dima.kotlarsky@gmail.com>
2025-01-11 22:31:59 -0500

    Update server.js
    adding option to read env variables

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -5,6 +7,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!!!!');
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
